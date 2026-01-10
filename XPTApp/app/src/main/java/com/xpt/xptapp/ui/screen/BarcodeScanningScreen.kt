4e88804df6f38639d2b5e6bebc0b677a61701bb2
Jetouille <jet.hots@gmail.com>
2026-01-10 14:59:31 +0100

    :construction: FloatingActionButton + preview

--- XPTApp/app/src/main/java/com/xpt/xptapp/ui/screen/BarcodeScanningScreen.kt
+++ XPTApp/app/src/main/java/com/xpt/xptapp/ui/screen/BarcodeScanningScreen.kt
@@ -9,8 +9,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.xpt.xptapp.ui.navigation.Screen
 import com.xpt.xptapp.ui.viewmodel.BarcodeScanningViewModel
 
@@ -34,4 +37,12 @@ fun BarcodeScanningScreen(navController: NavController,
             }
         }
     }
+}
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_4 // Use a predefined device
+)
+@Composable
+fun BarcodeScanningScreenPreview() {
+    BarcodeScanningScreen(rememberNavController())
 }
--- XPTApp/app/src/main/java/com/xpt/xptapp/ui/screen/PackageListScreen.kt
+++ XPTApp/app/src/main/java/com/xpt/xptapp/ui/screen/PackageListScreen.kt
@@ -3,16 +3,21 @@ package com.xpt.xptapp.ui.screen
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.xpt.xptapp.ui.navigation.Screen
 import com.xpt.xptapp.ui.viewmodel.PackageListViewModel
+import androidx.compose.ui.tooling.preview.Devices
 
 @Composable
 fun PackageListScreen(navController: NavController,
@@ -25,13 +30,22 @@ fun PackageListScreen(navController: NavController,
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
-            Button(
+            FloatingActionButton(
                 onClick = {
                     navController.navigate(Screen.BarcodeScanning.route)
                 }
             ) {
-                Text("Scanner")
+                Icon(Icons.Default.Add, contentDescription = "Add")
             }
         }
     }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_4 // Use a predefined device
+)
+@Composable
+fun PackageListScreenPreview() {
+    PackageListScreen(rememberNavController())
 }
